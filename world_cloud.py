4910e69f2167dfb99e0a4e81783ce36c68361dec
Anass Chahbouni <A.Chahbouni@hotmail.fr>
2018-11-27 15:05:41 +0100

    Create nuage de mots

---
+++ world_cloud.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 17:24:53 2018
+
+@author: Anass Chahbouni
+"""
+
+import os
+from os import path
+from wordcloud import WordCloud
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+# Read the whole text.
+text = open(path.join(d, 'C:\\Users\\etu\Desktop\\chapitre_1.txt')).read()
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
