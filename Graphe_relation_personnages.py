670789251da0bed45994e5528314aec4f98bee9f
Anass Chahbouni <A.Chahbouni@hotmail.fr>
2018-12-05 17:58:48 +0100

    AFFICHAGE GRAPHE AVEC POID

--- Graphe_relation_personnages.py
+++ Graphe_relation_personnages.py
@@ -22,43 +22,56 @@ def compareLists(sentenceList, majorCharacters):
 
 
 characterSentences=compareLists(pp.sentenceList, pp.majorCharacters)
+
 """ CREATION MATRICE ADJ COOCURENCE PERSONNAGE """
 vector1 = list()
 vector2 = list()
-for i in range(0,len(pp.majorCharacters)):
+for i in range(0,len(majorCharacters)):
     vector1.append(0)
-for j in range(0,len(pp.majorCharacters)):
+for j in range(0,len(majorCharacters)):
     vector2.append(vector1)
 matrice_perso=np.matrix(vector2)
 print(matrice_perso)
 """ ------------------ """
 
 import networkx as nx
+import matplotlib.pyplot as plt
 G = nx.Graph()
 
 """ REMPPLISSAGE MATRICE ADJ COOCURENCE PERSONNAGE + GRAPHE"""
 i=0
-for name in pp.majorCharacters:
-    w = 0
+for name in majorCharacters:
     for sentence in characterSentences[name]:
+        print("sentence : "+sentence)
         j=0
-        for name2 in pp.majorCharacters:
+        for name2 in majorCharacters:
             if name2 != name :
                 if sentence.find(name2)!=-1:
                     print(name,"-....-",name2)
                     matrice_perso[i,j]+=1
-                    w = w + 0.1
-                    print(matrice_perso)
-                    G.add_edge(name,name2,with_labels=True,weight=w)
-                    print(w)
             j+=1
     i+=1
-""" ----------------- """                    
+""" ----------------- """    
 
-""" affichage de la matrice """
-print(matrice_perso)
+""" REMPPLISSAGE  GRAPHE + AFFICHAGE GRAPHE AVEC POID """ 
 
-""" affichage du graphe (sans les poids pour l'instant) """
-nx.draw(G,with_labels=True, font_weight='bold')
-    
+""" ----------------- """  
+print(matrice_perso)                 
+i = 0
+for name in majorCharacters:
+    j = 0
+    for name2 in majorCharacters:
+        if name2 != name :
+            if matrice_perso[i,j] > 0:
+                w = 1 * matrice_perso[i,j]
+                print("nom1 : "+name+"| nom2 : "+name2+"| posI : "+str(i)+"| posJ : "+str(j)+"| weight : "+str(w))
+                G.add_edge(name,name2,weight=w)
+        j+=1
+    i+=1               
+
+pos=nx.spring_layout(G)
+nx.draw(G,pos)
+labels = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
 text.close()
