7d26e551f17d9625fae630ffc303012db591657f
Anass Chahbouni <A.Chahbouni@hotmail.fr>
2018-12-06 15:12:10 +0100

    Add extractEntityNames in PP
    extractEntityNames fonction

--- Personnage_principal.py
+++ Personnage_principal.py
@@ -1,3 +1,23 @@
+#Prend en entrée le tree et renvoie les entités nommées
+def extractEntityNames(tree, _entityNames=None):
+
+    if _entityNames is None:
+        _entityNames = []
+    try:
+        if nltk.__version__[0:2] == "2.":
+            label = tree.node
+        else:
+            label = tree.label()
+    except AttributeError:
+        pass
+    else:
+        if label == 'NE':
+            _entityNames.append(' '.join([child[0] for child in tree]))
+        else:
+            for child in tree:
+                extractEntityNames(child, _entityNames=_entityNames)
+    return _entityNames
+
 #Renvoie les personnages principaux
 def getMajorCharacters(entityNames):
     return {name for name in entityNames if entityNames.count(name) > 2}
